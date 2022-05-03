53847e62dbff3bc849cb789fff75aa5789cb9d73
Marcin Łyko <marcin.g.lyko@gmail.com>
2022-05-03 09:06:40 +0200

    Add some docstrings

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
+#!/usr/bin/python3
+
+"""A simple WEB application that handles two types of HTTP requests:
+  * /ping - POST requests accepting a JSON body containing a url key and
+    corresponding link. It sends a GET request to a received link and returns
+    a payload of that GET request
+  * /info - GET requests returning a hardcoded JSON response
+"""
+
+__author__ = 'Marcin Łyko'
+__email__ = 'marcin.g.lyko@gmail.com'
+
 import argparse
 
 import httpx
@@ -15,11 +27,15 @@ app = FastAPI()
 
 
 @app.post('/ping')
-async def handle_ping(ping: PingModel):
+async def handle_ping(ping: PingModel) -> Response:
+    """
+    Handles ping POST requests accepting a link in a JSON body.
+    """
     async with httpx.AsyncClient(verify=False, follow_redirects=1) as client:
         response: httpx.Response = await client.get(ping.url, timeout=10.0)
 
     if response.status_code == 200:
+        # Return payload when the GET request was succeed
         content_type = response.headers.get('Content-Type')
         return Response(response.content, status_code=200, media_type=content_type)
 
@@ -31,12 +47,15 @@ async def handle_ping(ping: PingModel):
 
 
 @app.get('/info')
-async def handle_info():
+async def handle_info() -> dict:
+    """
+    Handles info GET requests returning a hardcoded JSON body.
+    """
     return {'Receiver': 'Cisco is the best!'}
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument('-H', '--host',
                         help='TCP/IP hostname to serve on (default: %(default)r)',
                         default='localhost')
